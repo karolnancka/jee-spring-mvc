330d05719af5efeef333a342283b71d33163d101
karolnancka <karolnancka@gmail.com>
2022-04-24 15:51:17 +0200

    Session task 02

---
+++ src/main/java/pl/coderslab/Task02/Cart.java
@@ -0,0 +1,31 @@
+package pl.coderslab.Task02;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Scope(value = WebApplicationContext.SCOPE_SESSION,
+        proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class Cart {
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    public void addToCart(CartItem cartItem){
+        cartItems.add(cartItem);
+    }
+
+    public List<CartItem> getItems() {
+        return cartItems;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "cartItems=" + cartItems +
+                '}';
+    }
+}
---
+++ src/main/java/pl/coderslab/Task02/CartController.java
@@ -0,0 +1,41 @@
+package pl.coderslab.Task02;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+    private Cart cart;
+
+    public CartController(Cart cart){
+        this.cart = cart;
+    }
+
+    @GetMapping("/addtocart")
+    @ResponseBody
+    public String addtocart() {
+        Random rand = new Random();
+        cart.addToCart(new CartItem(1, new Product("prod" + rand.nextInt(10), rand.nextDouble())));
+        return "addtocart";
+    }
+
+
+    @GetMapping("/show")
+    @ResponseBody
+    public String show() {
+       String resultText = cart.getItems().stream()
+               .map(CartItem::toString)
+               .collect(Collectors.joining("</li><li>", "<ul><li>","</li></ul>"));
+
+       return resultText;
+    }
+
+
+}
